f3543deead379df8c171de3e3846de902ba513b1
Armen G <gharibi.armen@yahoo.com>
2019-12-15 18:30:32 -0800

    Added authentication request and response logic

--- src/main/java/com/agharibi/springsecurityjwt/config/SecurityConfigurer.java
+++ src/main/java/com/agharibi/springsecurityjwt/config/SecurityConfigurer.java
@@ -3,7 +3,9 @@ package com.agharibi.springsecurityjwt.config;
 import com.agharibi.springsecurityjwt.service.AppUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -18,6 +20,20 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(appUserDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/authenticate").permitAll()
+            .anyRequest().authenticated();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Bean
--- src/main/java/com/agharibi/springsecurityjwt/controllers/HomeController.java
+++ src/main/java/com/agharibi/springsecurityjwt/controllers/HomeController.java
@@ -1,13 +1,47 @@
 package com.agharibi.springsecurityjwt.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.agharibi.springsecurityjwt.models.AuthenticationRequest;
+import com.agharibi.springsecurityjwt.models.AuthenticationResponse;
+import com.agharibi.springsecurityjwt.service.AppUserDetailsService;
+import com.agharibi.springsecurityjwt.utils.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HomeController {
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private AppUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
     @GetMapping({"/index"})
     public String index() {
         return "Welcome to Spring Security JWT";
+    }
+
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                authenticationRequest.getUserName(), authenticationRequest.getPassword()));
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect userName or password", e);
+        }
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUserName());
+        final String jwtToken = this.jwtUtils.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse(jwtToken));
     }
 }
